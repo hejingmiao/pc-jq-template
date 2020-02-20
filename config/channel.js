e38a83b99715c5dad942376e68f41e514a69468a
贺淼淼 <hejingmiao@hemiaomiaodeMacBook-Pro.local>
2020-02-20 18:41:00 +0800

    init

---
+++ config/channel.js
@@ -0,0 +1,137 @@
+module.exports = {
+  "0001": {
+    "channelid": "0001",
+    "label": "新闻",
+    "value": "news"
+  }, 
+  "0003": {
+    "channelid": "0003",
+    "label": "娱乐",
+    "value": "ent"
+  }, 
+  "0005": {
+    "channelid": "0005",
+    "label": "体育",
+    "value": "sports"
+  }, 
+  "0006": {
+    "channelid": "0006",
+    "label": "旅游",
+    "value": "travel"
+  }, 
+  "0007": {
+    "channelid": "0007",
+    "label": "自营房产",
+    "value": "house"
+  }, 
+  "0008": {
+    "channelid": "0008",
+    "label": "汽车",
+    "value": "auto"
+  }, 
+  "0009": {
+    "channelid": "0009",
+    "label": "科技",
+    "value": "tech"
+  }, 
+  "0010": {
+    "channelid": "0010",
+    "label": "家居",
+    "value": "home"
+  }, 
+  "0011": {
+    "channelid": "0011",
+    "label": "手机",
+    "value": "mobile"
+  }, 
+  "0012": {
+    "channelid": "0012",
+    "label": "博客",
+    "value": "blog"
+  }, 
+  "0015": {
+    "channelid": "0015",
+    "label": "论坛",
+    "value": "bbs"
+  }, 
+  "0016": {
+    "channelid": "0016",
+    "label": "数码",
+    "value": "digi"
+  }, 
+  "0023": {
+    "channelid": "0023",
+    "label": "政务",
+    "value": "gov"
+  }, 
+  "0025": {
+    "channelid": "0025",
+    "label": "财经",
+    "value": "money"
+  }, 
+  "0026": {
+    "channelid": "0026",
+    "label": "女人",
+    "value": "lady"
+  }, {
+    "channelid": "0029",
+    "label": "教育",
+    "value": "edu"
+  }, {
+    "channelid": "0030",
+    "label": "跟贴",
+    "value": "tie"
+  }, {
+    "channelid": "0031",
+    "label": "游戏",
+    "value": "play"
+  }, {
+    "channelid": "0036",
+    "label": "亲子",
+    "value": "baby"
+  }, {
+    "channelid": "0037",
+    "label": "中产",
+    "value": "vhouse"
+  }, {
+    "channelid": "0038",
+    "label": "健康",
+    "value": "health"
+  }, {
+    "channelid": "0041",
+    "label": "收藏",
+    "value": "shoucang"
+  }, {
+    "channelid": "0042",
+    "label": "宠物",
+    "value": "pet"
+  }, {
+    "channelid": "0076",
+    "label": "传媒",
+    "value": "media"
+  }, {
+    "channelid": "0082",
+    "label": "酒",
+    "value": "jiu"
+  }, {
+    "channelid": "0085",
+    "label": "视频",
+    "value": "v"
+  }, {
+    "channelid": "0091",
+    "label": "校园",
+    "value": "daxue"
+  }, {
+    "channelid": "0093",
+    "label": "公益",
+    "value": "gongyi"
+  }, {
+    "channelid": "0095",
+    "label": "君子",
+    "value": "men"
+  }, {
+    "channelid": "0324",
+    "label": "佛学",
+    "value": "foxue"
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../css/index.less' // 引入css
+import './common.js' // 公共js
+// console.log($('.container'))
